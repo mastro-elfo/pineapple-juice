fb1d45e821af501b38b718219de5f70caa1cd9d4
mastro-elfo <francesco.209@gmail.com>
2016-05-23 22:06:38 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+(function(){
+	"use strict"
+	
+	window.addEvent('domready', function(){
+		// Create the collection obj
+		var collection = new window.Collection();
+		
+		// Contents are in conf object
+		new Request.JSON({
+			url: 'conf/conf.json',
+			onSuccess: function(json){
+				json.each(function(item){
+					// Populate collection
+					collection.addModel(item);
+				});
+				
+				// Display items
+				$$('.hidden').each(function(item){
+					item.removeClass('hidden');
+				})
+				$('waiter').addClass('hidden');
+			}
+		}).send();
+		
+		// Toggle button
+		new Epitome.View({
+			collection: collection,
+			element: 'maincontainer',
+			events: {
+				'click:relay(#toggle-menu)': 'toggle'
+			},
+			onToggle: function(event){
+				event && event.stop && event.stop();
+				if(this.element.hasClass('open')) {
+					this.element.removeClass('open');
+				}
+				else {
+					this.element.addClass('open');
+				}
+			},
+			'onChange:collection': function(){
+				this.element.removeClass('open');
+			}
+		});
+	});
+})();
---
+++ js/mvc.js
@@ -0,0 +1,120 @@
+
+(function(){
+	"use strict";
+	
+	window.Model = new Class({
+		Extends: Epitome.Model,
+		options: {
+			defaults: {
+				'name': 'Home M-E',
+				'description': 'Una home page qualsiasi',
+				'icon': '',
+				'title': 'Home page di mastro-elfo',
+				'src': 'http://mastro-elfo.github.io',
+				'language': 'it-IT',
+				
+				'selected': false
+			},
+			
+			'onChange:selected': function(value){
+				if(value){
+					this.collections.each(function(cll){
+						cll.filter(function(item){
+							return (item.cid != this.cid) && item.get('selected');
+						}, this).each(function(item){
+							item.set('selected', false);
+						})
+					}, this);
+				}
+			}
+		}
+	});
+	
+	window.Collection = new Class({
+		Extends: Epitome.Collection,
+		model: window.Model,
+		options: {
+			onAdd: function(model){
+				new window.MenuEntryView({
+					model: model,
+					element: new Element('div').inject($('menu'))
+				});
+				new window.TitleView({
+					model: model,
+					element: $('maincontainer').getElement('header')
+				});
+				new window.PageView({
+					model: model,
+					element: $('maincontainer').getElement('iframe')
+				});
+			}
+		}
+	});
+	
+	window.MenuEntryView = new Class({
+		Extends: Epitome.View,
+		options: {
+			template: '<div class="container" title="<%=title%>"><img src="<%=icon%>" alt="<%=icon-alt%>"/><div class="name"><%=name%></div><div class="description"><%=description%></div></div>',
+			events: {
+				'click': 'select'
+			},
+			onSelect: function(event){
+				event && event.stop && event.stop();
+				this.model.set('selected', true);
+			},
+			onReady: function(){
+				this.render();
+			},
+			'onChange:model': function(){
+				this.render();
+			},
+			'onDestroy:model': function(){
+				this.destroy();
+			}
+		},
+		render: function(){
+			this.empty();
+			this.element.set('html', this.template(this.model.toJSON()));
+			this.parent();
+			return this;
+		}
+	});
+	
+	window.TitleView = new Class({
+		Extends: Epitome.View,
+		options: {
+			onReady: function(){
+				this.render();
+			},
+			'onChange:model': function(){
+				this.render();
+			}
+		},
+		render: function(){
+			if(this.model.get('selected')) {
+				this.element.setStyles({
+					'color': this.model.get('header-color'),
+					'background-color': this.model.get('header-background')
+				});
+				this.element.getElement('#page-title').set('html', this.model.get('name'));
+			}
+		}
+	});
+	
+	window.PageView = new Class({
+		Extends: Epitome.View,
+		options: {
+			onReady: function(){
+				this.render();
+			},
+			'onChange:model': function(){
+				this.render();
+			}
+		},
+		render: function(){
+			if(this.model.get('selected')) {
+				this.element.set('src', this.model.get('src'));
+			}
+		}
+	});
+})();
